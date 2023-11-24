8141df44a418b6e650fab0570857f0bd9364a1d9
amirabas <abasaarafi@outlook.com>
2023-11-24 08:54:12 -0800

    commited addTodo

--- dist/todolist.js
+++ dist/todolist.js
@@ -1,1 +1,29 @@
+let inputvalue = document.querySelector('.todo-value');
+let addTodoBtn = document.querySelector('.add-todo');
+let todoList = document.querySelector('.todoList');
+const clickHandler = (e) => {
+    e.preventDefault();
+    if (inputvalue.value === '' || inputvalue.value.length < 3) {
+        alert('noting value adding');
+    }
+    else {
+        let newValueTdoo = {
+            id: crypto.randomUUID(),
+            title: inputvalue.value,
+            isCompleted: false
+        };
+        addTodoList(newValueTdoo);
+        console.log(newValueTdoo.title);
+    }
+};
+const addTodoList = (todo) => {
+    todoList.insertAdjacentHTML('beforeend', `
+  <li onclick="removeTodo('${todo.id}')">
+  ${todo.title}<span class="icon"
+    ><i class="fas fa-trash"></i
+  ></span>
+</li>
+  `);
+};
+addTodoBtn.addEventListener('click', (e) => (clickHandler(e)));
 //# sourceMappingURL=todolist.js.map
--- src/todolist.ts
+++ src/todolist.ts
@@ -1,3 +1,52 @@
+let inputvalue = document.querySelector('.todo-value') as HTMLInputElement;
+let addTodoBtn = document.querySelector('.add-todo') as HTMLButtonElement;
+let todoList = document.querySelector('.todoList') as HTMLDivElement;
 
+
+interface TypeTodo {
+  id: string,
+  title: string,
+  isCompleted : boolean
+}
+
+
+const clickHandler = (e:Event) => {
+
+  e.preventDefault()
+
+  if(inputvalue.value === '' || inputvalue.value.length < 3){
+    alert('noting value adding')
+  }else {
+    let newValueTdoo: TypeTodo =  {
+      id: crypto.randomUUID(),
+      title : inputvalue.value,
+      isCompleted : false
+      
+    } 
+    addTodoList(newValueTdoo)
+    console.log(newValueTdoo.title);
+  }
+
+  
+}
+
+
+
+const addTodoList = (todo:TypeTodo,)=>{
+  todoList.insertAdjacentHTML('beforeend', 
+  `
+  <li onclick="removeTodo('${todo.id}')">
+  ${todo.title}<span class="icon"
+    ><i class="fas fa-trash"></i
+  ></span>
+</li>
+  `
+  )
+}
+
+
+
+
+addTodoBtn.addEventListener('click',(e)=>(clickHandler(e)))
 
 
